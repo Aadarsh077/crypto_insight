d41efa5d671ff9466f756eaa35036ee0dfc7d5af
Aadarsh Krishnan <aadarshk777@gmail.com>
2024-06-06 19:02:05 +0530

    pagination

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { server } from "../index";
-import { Button, Container, HStack, Radio, RadioGroup } from "@chakra-ui/react";
+import { Container, HStack, Radio, RadioGroup } from "@chakra-ui/react";
 import Loader from "./Loader";
 import ErrorComponent from "./ErrorComponent";
 import CoinCard from "./CoinCard";
+import Pagination from "./Pagination"; 
 
 const Coins = () => {
   const [coins, setCoins] = useState([]);
@@ -12,6 +13,7 @@ const Coins = () => {
   const [error, setError] = useState(false);
   const [page, setPage] = useState(1);
   const [currency, setCurrency] = useState("inr");
+  const [totalPages, setTotalPages] = useState(132); 
 
   const currencySymbol =
     currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
@@ -20,8 +22,6 @@ const Coins = () => {
     setPage(page);
     setLoading(true);
   };
-
-  const btns = new Array(132).fill(1);
 
   useEffect(() => {
     const fetchCoins = async () => {
@@ -69,18 +69,11 @@ const Coins = () => {
             ))}
           </HStack>
 
-          <HStack w={"full"} overflowX={"auto"} p={"8"}>
-            {btns.map((item, index) => (
-              <Button
-                key={index}
-                bgColor={"blackAlpha.900"}
-                color={"white"}
-                onClick={() => changePage(index + 1)}
-              >
-                {index + 1}
-              </Button>
-            ))}
-          </HStack>
+          <Pagination
+            currentPage={page}
+            totalPages={totalPages}
+            onPageChange={changePage}
+          />
         </>
       )}
     </Container>
